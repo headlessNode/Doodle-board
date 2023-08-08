a546b74bbf7781b9d0cd40204e20f1a192f3180f
headlessNode <harriskhan047@outlook.com>
2023-08-08 14:41:00 +0500

    Add features - Implement button click and prompt for handling grid dimension update. - Implement button click function to clear the grid.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,65 @@
-// GRID CONTAINER
-    var gridContainer = document.querySelector('.grid-container');
+// Get a reference to the grid-container element
+var gridContainer = document.querySelector('.grid-container');
+gridContainer.style.height = '500px';
+gridContainer.style.width = '500px';
 
-//  CREATE 16x16 GRID
-    var nCells = 16;
-    var nRows = 16;
+var nCells = 16; // Default number of cells
+var nRows = 16; // Default number of rows
 
-    function createGrid(nRows){
-        for (var rows = 0; rows<nRows; rows++){
-            var row = document.createElement('div');
-            row.classList.add('row-'+rows);
-            row.id = 'rows';
+// Function to create a row of grid cells
+function createRow(nCells) {
+    var gridRow = document.createElement('div');
+    gridRow.classList.add('grid-row');
+    gridContainer.appendChild(gridRow);
+    for (var i = 0; i < nCells; i++) {
+        var gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.style.width = (gridContainer.clientWidth / nCells) + 'px';
+        gridCell.style.height = (gridContainer.clientHeight / nRows) + 'px';
+        gridRow.appendChild(gridCell);
 
-            for(var i = 0; i<nCells; i++){
-                var cell = document.createElement('div');
-                cell.classList.add('cell');
-                row.appendChild(cell);
-            }
-
-            gridContainer.appendChild(row);
-        }
+        gridCell.addEventListener('mouseover', function (e) {
+            e.target.style.backgroundColor = 'red';
+        });
     }
+}
 
+// Function to create the entire grid
+function createGrid(nCells, nRows) {
+    for (var i = 0; i < nRows; i++) {
+        createRow(nCells);
+    }
+}
 
-    createGrid(nRows);
+// Function to handle the button click and update grid dimensions
+function getPxls(e) {
+    var Pixels = prompt('Enter number of pixels per side (max:100)', '16');
+    var nPixels = parseInt(Pixels);
+    nCells = nPixels;
+    nRows = nPixels;
+
+    if (Pixels != null) {
+        gridContainer.innerHTML = ''; // Clear the previous grid
+        createGrid(nCells, nRows);    // Recreate the grid with new dimensions
+    }
+}
+
+function clear() {
+    var clrGrid = document.querySelectorAll('.grid-cell');
+    for (var i = 0; i < nCells * nRows; i++) {
+        clrGrid[i].style.backgroundColor = 'white';
+    }
+}
+
+// Function to set up the initial state of the grid
+function paint() {
+    var pixels = document.querySelector('.pxl-btn');
+    pixels.addEventListener('click', getPxls);
+
+    var clrBoard = document.querySelector('.clr-btn');
+    clrBoard.addEventListener('click', clear);
+
+    createGrid(nCells, nRows);
+}
+
+paint();
